15c5799bac258ed94be9bec46a0e59702644eca1
victtorciferri <95662932+victtorciferri@users.noreply.github.com>
2025-05-02 17:17:19 +0000

    Save Replit version for comparison with GCP

--- server/index.ts
+++ server/index.ts
@@ -294,9 +294,10 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // Use the PORT environment variable for Cloud Run compatibility
-  // or default to 5000 for local development
-  const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+  // ALWAYS serve the app on port 5000
+  // this serves both the API and the client.
+  // It is the only port that is not firewalled.
+  const port = 5000;
   server.listen({
     port,
     host: "0.0.0.0",
